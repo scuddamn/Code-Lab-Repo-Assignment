e113aab5c675e1011d7cbb631e8603358df1bdb0
scuddamn <aescudder@gmail.com>
2019-11-11 12:18:15 -0500

    Added bouncing ball and sun cycle

---
+++ Code Lab Repo Assignment/Assets/Scripts/SunCycleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunCycleScript : MonoBehaviour
+{
+    public float Speed;
+
+
+    
+
+    // Make sun and moon rotate around a central axis, creating effect of sunrise and set
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, Speed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+}
